a1b364328b5cc1352b905d62c54c44b88eb42de9
aria <ariarahman010@gmail.com>
2025-05-11 22:26:33 +0600

    adding php file

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])) {
+        echo "<h2>Form Data Received:</h2>";
+        echo "<strong>Username:</strong> " . $_POST['USERname'] . "<br>";
+        echo "<strong>Email:</strong> " . $_POST['email'] . "<br>";
+        echo "<strong>Date of Birth:</strong> " . $_POST['DOB'] . "<br>";
+        echo "<strong>Gender:</strong> " . $_POST['gender'] . "<br>";
+        echo "<strong>Country:</strong> " . $_POST['country'] . "<br>";
+        echo "<strong>AQI:</strong> " . $_POST['AQI'] . "<br>";
+        echo "<strong>Password:</strong> " . $_POST['password'] . "<br>";
+        echo "<strong>Background Color:</strong> " . $_POST['color'] . "<br>";
+    
+    }
+ else {
+    echo "No data submitted.";
+}
+?>
